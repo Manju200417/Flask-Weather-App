cd423d147a66e29d7ed3e4f2e3dfae5a809e30c5
Manjunath <121750296+Manju200417@users.noreply.github.com>
2025-08-07 20:28:45 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import os
 
 load_dotenv()
 
-API_KEY = os.getenv("WEATHER_API_KEY")
+API_KEY = os.getenv("API_KEY")
 
 app = Flask(__name__)
 
@@ -39,4 +39,5 @@ def main():
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=False)
+
 
